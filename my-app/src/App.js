27dc7b36280632bd9f8e682847ce5c21c3e4407e
Ajit-Ravan <ravanajit110@gmail.com>
2024-03-08 23:05:50 +0530

    toggle mode to entire website , Detailed js styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import About from "./About";
 import "./App.css";
 
 import Navbar from "./Navbar";
 import TextForms from "./TextForms";
+import React from "react";
+
 function App() {
+  //Adding dark mode and light mode functionality to our entire project
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#0d0833';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+    console.log(mode);
+  };
   return (
     <>
-      <Navbar title="Desi QnA" link ='links' />   
-      <TextForms heading = "Enter the text to modify: "/>
-      <About/>
+      <Navbar title="Desi QnA" link="links" mode={mode} toggleMode={toggleMode} />
+      <TextForms heading="Enter the text to modify: " mode={mode} />
+      <About mode={mode} />
     </>
   );
 }
--- my-app/src/TextForms.js
+++ my-app/src/TextForms.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 export default function TextForms(props) {
 
-    
-    const handleUpClick = () => {
-        console.log("upper case was clicked " + text);
-      // setText("You clicked on the button handleUpClick");
-      //CONVERTING TO UPPERCASE
-      let upperText = text.toUpperCase();
-      setText(upperText);
+
+  const handleUpClick = () => {
+    console.log("upper case was clicked " + text);
+    // setText("You clicked on the button handleUpClick");
+    //CONVERTING TO UPPERCASE
+    let upperText = text.toUpperCase();
+    setText(upperText);
   }
   const handleToLowClick = () => {
     console.log("lower case was clicked " + text);
@@ -17,39 +17,45 @@ export default function TextForms(props) {
     let lowerText = text.toLowerCase();
     setText(lowerText);
   };
-    const handleOnChange = (event) => {
-        setText(event.target.value);
-        console.log("on change function");
-    }
-    const [text, setText] = useState("");
-    // setText("this is new text from settext");
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+    console.log("on change function");
+  }
+  const [text, setText] = useState("");
+  // setText("this is new text from settext");
   // console.log(text);
-  
+
   const handleDelete = () => {
     setText("");
     console.log("content has been deleted");
   }
   return (
     <>
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text} onChange={handleOnChange}></textarea>
-        </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>To uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleToLowClick}>To Lowercase</button>
-        <button type="button" class="btn btn-secondary mx-2" onClick={handleDelete}>Delete Content</button>
-      </div>
+      <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <div>
+          <h1>{props.heading}</h1>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={text}
+              onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
+          </div>
 
-      <div className="container my-4">
-        <h1>Yout Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{ 0.008*text.split(" ").length} minutes reading</p>
+          <button className="btn btn-primary mx-2" onClick={handleUpClick}>To uppercase</button>
+          <button className="btn btn-primary mx-2" onClick={handleToLowClick}>To Lowercase</button>
+          <button type="button" className="btn btn-secondary mx-2" onClick={handleDelete}>Delete Content</button>
+
+        </div>
+
+        <div className="container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+          <h1>Yout Text Summary</h1>
+          <p>{text.split(" ").length} words and {text.length} characters</p>
+          <p>{0.008 * text.split(" ").length} minutes reading</p>
+
+          <h1>Preview :</h1>
+          <p>{text.length > 0 ? text : "Enter the text:"}</p>
+
+        </div>
       </div>
-      
-        <h1>Preview :</h1>
-        <p>{text}</p>
-      </>
+    </>
   )
 }
